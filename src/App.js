af3cec21e3fb61365585da6a44a9712d419ed917
Daniel Otabor <danieldamianotabor@gmail.com>
2022-08-19 12:28:56 +0100

    Scroll to restaurants function

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import {getPlacesData } from './api';
 
 function App() {
   const [places, setPlaces] = useState([]);
-
+  const [childClicked, setChildClicked]= useState(null);
   const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState({});
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(()=>{
     navigator.geolocation.getCurrentposition(({ cords: {latitude, longitude}}) => {
@@ -18,25 +19,32 @@ function App() {
     })
   }, []);
   useEffect(() => {
+    setIsLoading(true);
     getPlacesData()
       .then((datat) => {
         console.log(data);
         setPlaces(data);
+        setIsLoading(false);
       })
   }, [coordinates, bounds]);
   return (
-    <>
+    <>s
         <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{width: '100%' }}>
         <Grid item xs={12} md = {4}>
-          <List places={places}/>
+          <List 
+              places={places}/>
+              childClicked={childClicked}
+              isLoading={isLoading}
         </Grid>
         <Grid item xs={12} md={8}>
           <Map 
             setCordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
         </Grid>
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -4,8 +4,9 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
-export default function PlaceDetails() {
+export default function PlaceDetails({place, selected, refProp}) {
   const classes = useStyles();
+  if(selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: "start"})
   return (
     <Card elevation={6}>
       <CardMedia
@@ -16,8 +17,8 @@ export default function PlaceDetails() {
       <CardContent>
         <Typography gutterBottom variant="h5">{place.name}</Typography>
         <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+          <Rating size="small" value={Number(place.rating)} readOnly />         
+          <Typography gutterBottom variant="subtitle1">"out of {place.num_reviews} reviews</Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Ranking</Typography>
